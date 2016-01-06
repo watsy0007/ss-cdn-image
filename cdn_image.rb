2a5d68f4d8d6a01e90693b871d2dba8aed1babdb
watsy0007 <watsy0007@gmail.com>
2016-01-07 01:19:40 +0800

    update

--- cdn_image.rb
+++ cdn_image.rb
@@ -22,19 +22,12 @@ Cuba.define do
   on post do
     on "/" do
       file = param('file').call
-      puts file[0]
-      open(file[0][:tempfile]) do |file|
-        puts file.size
-      end
       code, result, response_headers = Qiniu::Storage.upload_with_put_policy(
-    put_policy,     # 上传策略
-    file[0][:tempfile]
-)
-      puts code, result, response_headers
-      #res.write req.to_s
+		put_policy,     # 上传策略
+    		file[0][:tempfile]
+	)	
       res['Content-Type'] = 'application/json'
       result = "{\"key\": \"#{result['key']}\"}"
-      puts result
       res.write result
     end
   end
